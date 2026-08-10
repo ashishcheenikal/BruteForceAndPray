82c7dcaebbefea6e08f0bc1c966045318fb3a069
ashishcheenikal <ashishcheenikal@gmail.com>
2026-08-10 22:40:38 +0530

    13. Best Time to Buy and Sell Stock LC121

---
+++ cpp/arrays/buy_sell_stock.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int maxProfit(vector<int> &prices)
+{
+    int n = prices.size();
+
+    int maxProfit = 0;
+    int smallest = prices[0];
+    for (int i = 0; i < n; i++)
+    {
+        int profit = prices[i] - smallest;
+
+        if (prices[i] < smallest)
+        {
+            smallest = prices[i];
+        }
+
+        if (profit > maxProfit)
+        {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        cout << maxProfit(arr) << '\n';
+    }
+
+    return 0;
+}
